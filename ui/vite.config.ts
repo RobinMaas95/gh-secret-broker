57fb04812ff715d7add3a1c17a68f1421759c0d8
Robin Maasjosthusmann <robin@maasjosthusmann.de>
2025-12-07 10:26:45 +0100

    deps: Migrate to tailwind v4

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -2,13 +2,15 @@ import path from "path";
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [svelte(), tailwindcss()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
-      "$lib": path.resolve(__dirname, "./src/lib"),
+      "@": path.resolve(process.cwd(), "./src"),
+      "$lib": path.resolve(process.cwd(), "./src/lib"),
     },
   },
   build: {
